a53e61776eec9602734b1d6184a1fd4cb6a89360
johnrobertmcc <john.robert.mcc@gmail.com>
2020-10-28 11:42:54 -0400

    background

--- src/index.js
+++ src/index.js
@@ -207,56 +207,56 @@ function letHimRest() {
     }
 }
 //FOR PARTICLE CANVAS
-                    // function Particle(x, y, dX, dY, size, color){
-                    //     this.x = x;
-                    //     this.y = y;
-                    //     this.dX = dX;
-                    //     this.dY = dY;
-                    //     this.size = size;
-                    //     this.color = color;
-                    // }
+                    function Particle(x, y, dX, dY, size, color){
+                        this.x = x;
+                        this.y = y;
+                        this.dX = dX;
+                        this.dY = dY;
+                        this.size = size;
+                        this.color = color;
+                    }
 
-                    // let particleArray;
+                    let particleArray;
 
-                    // Particle.prototype.draw = function(){
-                    //     ctx.beginPath();
-                    //     ctx.arc(this.x, this.y, this.size, 0, Math.PI*2, false);
-                    //     ctx.fillStyle = "rgba(255, 255, 0, 0.2)"
-                    //     ctx.alpha = 0.4
-                    //     ctx.fill();
-                    // }
+                    Particle.prototype.draw = function(){
+                        ctx.beginPath();
+                        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2, false);
+                        ctx.fillStyle = "rgba(255, 255, 0, 0.2)"
+                        ctx.alpha = 0.4
+                        ctx.fill();
+                    }
 
-                    // Particle.prototype.update = function() {
-                    //     if(this.x + this.size > canvas.width || this.x - this.size < 0){
-                    //         this.dX = -this.dX;
-                    //     }
-                    //     if(this.y + this.size > canvas.height || this.y - this.size < 0){
-                    //         this.dY = -this.dY;
-                    //     }
+                    Particle.prototype.update = function() {
+                        if(this.x + this.size > canvas.width || this.x - this.size < 0){
+                            this.dX = -this.dX;
+                        }
+                        if(this.y + this.size > canvas.height || this.y - this.size < 0){
+                            this.dY = -this.dY;
+                        }
                         
-                    //     this.x += this.dX;
-                    //     this.y += this.dY;
-                    //     this.draw();
-                    // }
+                        this.x += this.dX;
+                        this.y += this.dY;
+                        this.draw();
+                    }
 
-                    // function init(){
-                    //     particleArray = [];
-                    //     for(let i = 0; i < 100; i++){
-                    //         let size = Math.floor(Math.random() * 6 + 1)
-                    //         let x = Math.random() * (innerWidth - size * 2);
-                    //         let y = Math.random() * (innerHeight - size * 2);
-                    //         let dX = (Math.random() * .4) - .5;
-                    //         let dY = (Math.random() * .4) - .5;
-                    //         let color = 'white'
-                    //         particleArray.push(new Particle(x, y, dX, dY, size, color))
+                    function init(){
+                        particleArray = [];
+                        for(let i = 0; i < 100; i++){
+                            let size = Math.floor(Math.random() * 6 + 1)
+                            let x = Math.random() * (innerWidth - size * 2);
+                            let y = Math.random() * (innerHeight - size * 2);
+                            let dX = (Math.random() * .4) - .5;
+                            let dY = (Math.random() * .4) - .5;
+                            let color = 'white'
+                            particleArray.push(new Particle(x, y, dX, dY, size, color))
 
-                    //     }
+                        }
 
-                    // }
+                    }
 
 
 
-                    // // const particle1 = new Particle(10,10,1,1,5,'white');
+                    const particle1 = new Particle(10,10,1,1,5,'white');
 
 
 let bgn_idx = 0;
@@ -321,9 +321,9 @@ function animate() {
 
         //for particles -- 
 
-        // for(i = 0; i < particleArray.length; i ++){
-        //     particleArray[i].update();
-        // }
+        for(i = 0; i < particleArray.length; i ++){
+            particleArray[i].update();
+        }
         
         moveThisLad();
         makeHimWalk();
@@ -341,7 +341,7 @@ function animate() {
 
 
 //FOR PARTICLES
-// init();
+init();
 
 
 animation(20);
