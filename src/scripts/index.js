b9247864e8ab9ec31a7468f2113931340786026e
johnrobertmcc <john.robert.mcc@gmail.com>
2020-12-01 17:41:43 -0500

    change it up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -318,7 +318,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 3
+        speed: 3,
+        id: 0
     },
     mouse2 : {
         img: mouseImg, 
@@ -329,7 +330,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 6
+        speed: 6,
+        id: 1
     },
     mouse3 : {
         img: mouseImg, 
@@ -340,7 +342,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 8
+        speed: 8,
+        id: 2
     },
     mouse4 : {
         img: mouseImg, 
@@ -351,7 +354,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 6
+        speed: 6,
+        id: 3
     },
     mouse5 : {
         img: mouseImg, 
@@ -362,7 +366,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 9
+        speed: 9,
+        id: 4
     },
     mouse6 : {
         img: mouseImg, 
@@ -373,7 +378,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 10
+        speed: 10,
+        id: 5
     },
     mouse7 : {
         img: mouseImg, 
@@ -384,7 +390,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 6
+        speed: 6,
+        id: 5
     },
     mouse8 : {
         img: mouseImg, 
@@ -395,7 +402,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 9
+        speed: 9,
+        id: 6
     },
     mouse9 : {
         img: mouseImg, 
@@ -406,7 +414,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 10
+        speed: 10,
+        id: 7
     },
     mouse10 : {
         img: mouseImg, 
@@ -417,7 +426,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 6
+        speed: 6,
+        id: 7
     },
     mouse11 : {
         img: mouseImg, 
@@ -428,7 +438,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 8
+        speed: 8,
+        id: 8
     },
     mouse12 : {
         img: mouseImg, 
@@ -439,7 +450,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 5
+        speed: 5,
+        id: 9
     },
     mouse13 : {
         img: mouseImg, 
@@ -450,7 +462,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 5
+        speed: 5,
+        id: 10
     },
     mouse14 : {
         img: mouseImg, 
@@ -461,7 +474,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 6
+        speed: 6,
+        id: 11
     },
     mouse15 : {
         img: mouseImg, 
@@ -472,7 +486,8 @@ const mice = {
         frameX: 0,
         frameY: 1,
         position: 0,
-        speed: 6
+        speed: 6,
+        id: 12
     },
 
 }
@@ -506,11 +521,10 @@ function moveThatMouse(){
         }else{
             arr[i].frameX = 0;
         }
-        // if(arr[i].x > -200){
             arr[i].x -= arr[i].speed;
             arr[i].x --;
         }
-    }
+  }
 
 
 
@@ -523,15 +537,15 @@ function moveThatMouse(){
 
 //collision.js
 
-function collision(player, obstacle) {
+function collision(rect1, rect2) {
   
-  if (player.x + player.w == obstacle.x &&
-      obstacle.y == player.y + player.h) {
-        debugger
-        console.log('hit');
-  } else {
-    return false;
-  }
+  if (rect1.x < rect2.x + rect2.w &&
+   rect1.x + rect1.w > rect2.x &&
+   rect1.y < rect2.y + rect2.h &&
+   rect1.y + rect1.h > rect2.y &&
+   !player.jumping) {
+     return true
+}
 };
 
 function collisionCheck(){
@@ -544,7 +558,9 @@ function collisionCheck(){
         // enterGame();
         // }
 
-        collision(player, arr[i])
+        if(collision(player, arr[i])){
+          delete arr[i]
+        }
     }
 }
 
@@ -557,7 +573,7 @@ function animation(fps){
     animate();
 }
 
-function animate() { //MAIN GAME
+function animate() {
     
     requestAnimationFrame(animate);
     now = Date.now();
@@ -590,7 +606,6 @@ function animate() { //MAIN GAME
       
         
         moveThisLad();
-        // drawObstacle('white');
         makeHimWalk();
         applyGravity();
         // ohLordHeRunninNJumpin();
@@ -631,6 +646,7 @@ function enterGame(){
         const audio = document.querySelector("audio");
         audio.volume = 0.2;
         audio.play();
+        player.moving = false;
     }else if(mode == 2){
         gameOver();
         
@@ -638,163 +654,3 @@ function enterGame(){
 }
 
 enterGame();
-
-
-
-
-
-// const context = document.querySelector("canvas").getContext("2d");
-
-// context.canvas.height = 400;
-// context.canvas.width = 1220;
-
-// // Start the frame count at 1
-// let frameCount = 1;
-// // Set the number of obstacles to match the current "level"
-// let obCount = frameCount;
-// // Create a collection to hold the generated x coordinates
-// const obXCoors = [];
-
-// const square = {
-
-//   height: 32,
-//   jumping: true,
-//   width: 32,
-//   x: 0,
-//   xVelocity: 0,
-//   y: 0,
-//   yVelocity: 0
-
-// };
-// // Create the obstacles for each frame
-// const nextFrame = () => {
-//   // increase the frame / "level" count
-//   frameCount++;
-  
-//   for (let i = 0; i < obCount; i++) {
-//     // Randomly generate the x coordinate for the top corner start of the triangles
-//     let obXCoor = Math.floor(Math.random() * (1165 - 140 + 1) + 140);
-//     obXCoors.push(obXCoor);
-//   }
-
-// }
-
-// const controller = {
-
-//   left: false,
-//   right: false,
-//   up: false,
-//   keyListener: function (event) {
-
-//     var key_state = (event.type == "keydown") ? true : false;
-
-//     switch (event.keyCode) {
-
-//       case 37:// left key
-//         controller.left = key_state;
-//         break;
-//       case 38:// up key
-//         controller.up = key_state;
-//         break;
-//       case 39:// right key
-//         controller.right = key_state;
-//         break;
-
-//     }
-
-//   }
-
-// };
-
-// const loop = function () {
-
-//   if (controller.up && square.jumping == false) {
-
-//     square.yVelocity -= 20;
-//     square.jumping = true;
-
-//   }
-
-//   if (controller.left) {
-
-//     square.xVelocity -= 0.5;
-
-//   }
-
-//   if (controller.right) {
-
-//     square.xVelocity += 0.5;
-
-//   }
-
-//   square.yVelocity += 1.5;// gravity
-//   square.x += square.xVelocity;
-//   square.y += square.yVelocity;
-//   square.xVelocity *= 0.9;// friction
-//   square.yVelocity *= 0.9;// friction
-
-//   // if square is falling below floor line
-//   if (square.y > 386 - 16 - 32) {
-
-//     square.jumping = false;
-//     square.y = 386 - 16 - 32;
-//     square.yVelocity = 0;
-
-//   }
-
-//   // if square is going off the left of the screen
-//   if (square.x < -20) {
-
-//     square.x = 1220;
-
-//   } else if (square.x > 1220) {// if square goes past right boundary
-
-//     square.x = -20;
-//     nextFrame();
-
-//   }
-//   // Creates the backdrop for each frame
-//   context.fillStyle = "#201A23";
-//   context.fillRect(0, 0, 1220, 400); // x, y, width, height
-
-
-//   // Creates and fills the cube for each frame
-//   context.fillStyle = "#8DAA9D"; // hex for cube color
-//   context.beginPath();
-//   context.rect(square.x, square.y, square.width, square.height);
-//   context.fill();
-
-
-//   // Create the obstacles for each frame
-//   // Set the standard obstacle height
-//   const height = 200 * Math.cos(Math.PI / 6);
-
-//   context.fillStyle = "#FBF5F3"; // hex for triangle color
-//   obXCoors.forEach((obXCoor) => {
-//     context.beginPath();
-
-//     context.moveTo(obXCoor, 385); // x = random, y = coor. on "ground"
-//     context.lineTo(obXCoor + 20, 385); // x = ^random + 20, y = coor. on "ground"
-//     context.lineTo(obXCoor + 10, 510 - height); // x = ^random + 10, y = peak of triangle
-  
-//     context.closePath();
-//     context.fill();
-//   })
-
-
-//   // Creates the "ground" for each frame
-//   context.strokeStyle = "#2E2532";
-//   context.lineWidth = 30;
-//   context.beginPath();
-//   context.moveTo(0, 385);
-//   context.lineTo(1220, 385);
-//   context.stroke();
-
-//   // call update when the browser is ready to draw again
-//   window.requestAnimationFrame(loop);
-
-// };
-
-// window.addEventListener("keydown", controller.keyListener)
-// window.addEventListener("keyup", controller.keyListener);
-// window.requestAnimationFrame(loop);
